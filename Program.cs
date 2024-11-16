742de80897281f9f7c58af129159780f37d3b8fb
NT-000 <nicolai.thue.reier@gmail.com>
2024-11-16 15:05:49 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿        int number = 3;
+        long longNum = 333333333333L;
+        float floatNum = 3.22f;
+        decimal decNum = 33.33m;
+        double doubleNum = 33.33;
+        string varString = "Hallo";
+        char varChar = 'a';
+        bool varBool = true;
+
+
+        Console.WriteLine($"Number: {number}");
+        Console.WriteLine($"longNum: {longNum}");
+        Console.WriteLine($"floatNum: {floatNum}");
+        Console.WriteLine($"decNum: {decNum}");
+        Console.WriteLine($"doubleNum: {doubleNum}");
+        Console.WriteLine($"varString: {varString}");
+        Console.WriteLine($"char: {varChar}");
+        Console.WriteLine($"bool: {varBool}");
